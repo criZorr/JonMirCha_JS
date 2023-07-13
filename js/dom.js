47e576f03026b1d65ba2662ac27cf10c6b8a605a
criZorr <christophermjk@outlook.com>
2023-07-13 16:59:33 -0500

    02 - DOM: Nodos, Elementos y Selectores

--- js/dom.js
+++ js/dom.js
@@ -29,3 +29,29 @@
 
     console.groupEnd();
 }
+
+//Nodos, Elementos y Selectores
+{
+    console.group("02 - Nodos, Elementos y Selectores");
+
+    console.log(document.getElementsByTagName("li"));
+    console.log(document.getElementsByClassName("card"));
+    console.log(document.getElementsByName("nombre"));
+
+    console.log(document.getElementById("menu"));
+
+    console.log(document.querySelector("#menu"));
+    console.log(document.querySelector("a"));
+    console.log(document.querySelectorAll("a"));
+    console.log(document.querySelectorAll("a").length);
+    document.querySelectorAll("a").forEach(el => {console.log(el);});
+
+    console.log(document.querySelector(".card"));
+    console.log(document.querySelectorAll(".card"));
+    console.log(document.querySelectorAll(".card")[2]);
+
+    console.log(document.querySelector("#menu li"));
+    console.log(document.querySelectorAll("#menu li"));
+
+    console.groupEnd();
+}
