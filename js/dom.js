3f070428f0be8e48ff745570202d1a4bb498f6b8
criZorr <christophermjk@outlook.com>
2023-07-19 14:56:57 -0500

    07 - DOM Traversing: Recorriendo el DOM

--- js/dom.js
+++ js/dom.js
@@ -203,3 +203,24 @@
 
     console.groupEnd();
 }
+
+//DOM Traversing: Recorriendo el DOM
+{
+    console.group("07 - DOM Traversing: Recorriendo el DOM");
+
+    const $cards = document.querySelector(".cards");
+
+    console.log($cards);
+    console.log($cards.children);
+    console.log($cards.children[2]);
+    console.log($cards.parentElement);
+    console.log($cards.firstChild);
+    console.log($cards.lastChild);
+    console.log($cards.previousElementSibling);
+    console.log($cards.nextElementSibling);
+    console.log($cards.closest("div"));
+    console.log($cards.closest("body"));
+    console.log($cards.children[3].closest("section"));
+
+    console.groupEnd();
+}
