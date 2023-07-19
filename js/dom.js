9b24c8d5aa454131c8ef8b0ca035c6cf8409e42f
criZorr <christophermjk@outlook.com>
2023-07-19 15:48:09 -0500

    08 - Creando Elementos y Fragmentos

--- js/dom.js
+++ js/dom.js
@@ -224,3 +224,71 @@
 
     console.groupEnd();
 }
+
+//Creando Elementos y Fragmentos
+{
+    console.group("08 - Creando Elementos y Fragmentos");
+
+    const $figure = document.createElement("figure"),
+    $img = document.createElement("img"),
+    $figcaption = document.createElement("figcaption"),
+    $figcaptionText = document.createTextNode("Animals"),
+    $cards = document.querySelector(".cards"),
+    $figure2 = document.createElement("figure");
+
+    $img.setAttribute("src", "https://picsum.photos/200/200?animals");
+    $img.setAttribute("alt", "Animals");
+    $figure.classList.add("card");
+
+    $figcaption.appendChild($figcaptionText);
+    
+    $figure.appendChild($img);
+    $figure.appendChild($figcaption);
+
+    $cards.appendChild($figure);
+
+    $figure2.innerHTML = `
+    <img src="https://picsum.photos/200/200?people" alt="People">
+    <figcaption>People</figcaption>
+    `;
+    $figure2.classList.add("card");
+
+    $cards.appendChild($figure2);
+
+    const estaciones = ["Primavera", "Verano", "Otoño", "Invierno"],
+    $ul = document.createElement("ul");
+
+    document.write("<h3>Estaciones del año</h3>");
+    document.body.appendChild($ul);
+
+    estaciones.forEach(el => {
+        const $li = document.createElement("li");
+        $li.textContent = el;
+        $ul.appendChild($li);
+    });
+
+    const continentes = ["África", "América", "Asia", "Europa", "Oceanía"],
+    $ul2 = document.createElement("ul");
+
+    document.write("<h3>Continentes del mundo</h3>");
+    document.body.appendChild($ul2);
+
+    $ul2.innerHTML = "";
+    continentes.forEach(el => $ul2.innerHTML += `<li>${el}</li>`);
+
+    const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "setiembre", "octubre", "noviembre", "diciembre"],
+    $ul3 = document.createElement("ul"),
+    $fragment = document.createDocumentFragment();
+
+    meses.forEach(el => {
+        const $li = document.createElement("li");
+        $li.textContent = el;
+        $fragment.appendChild($li);
+    });
+
+    document.write("Meses del año");
+    $ul3.appendChild($fragment);
+    document.body.appendChild($ul3);
+
+    console.groupEnd();
+}
