8ea54aebb341ad0665c81ba0b478b8270e980fac
criZorr <christophermjk@outlook.com>
2023-07-19 16:46:31 -0500

    10 - Modificando Elementos (Old Style)

--- js/dom.js
+++ js/dom.js
@@ -333,6 +333,30 @@
     });
 
     $cards.appendChild($fragment9);
-    
+
+    console.groupEnd();
+}
+
+//Modificando Elementos (Old Style)
+{
+    console.group("10 - Modificando Elementos (Old Style)");
+
+    const $cards = document.querySelector(".cards"),
+    $newCard = document.createElement("figure")
+    $cloneCards = $cards.cloneNode(true);
+
+    $newCard.innerHTML=`
+    <img src="https://picsum.photos/200/200?any" alt="Any">
+    <figcaption>Any</figcaption>
+    `;
+
+    $newCard.classList.add("card");
+
+    $cards.replaceChild($newCard, $cards.children[2]);
+    $cards.insertBefore($newCard, $cards.firstElementChild);
+    $cards.removeChild($cards.lastElementChild);
+
+    document.body.appendChild($cloneCards);
+
     console.groupEnd();
 }
