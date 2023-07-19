20ca8437f0e1a175f8b90ab7cc03e2c4e3913c35
criZorr <christophermjk@outlook.com>
2023-07-19 10:58:24 -0500

    Estilos y variables CSS

--- js/dom.js
+++ js/dom.js
@@ -93,3 +93,50 @@
 
     console.groupEnd();
 }
+
+//console.group("04 - Estilos y variables CSS")
+{
+    console.group("04 - Estilos y variables CSS");
+
+    const $linkDOM = document.querySelector(".link-dom");
+
+    console.log($linkDOM.style);
+    console.log($linkDOM.getAttribute("style"));
+    console.log($linkDOM.style.backgroundColor);
+    console.log($linkDOM.style.color);
+    console.log(window.getComputedStyle($linkDOM));
+    console.log(getComputedStyle($linkDOM).getPropertyValue("color"));
+
+    $linkDOM.style.setProperty("text-decoration", "none");
+    $linkDOM.style.setProperty("display", "block");
+    $linkDOM.style.width = "50%";
+    $linkDOM.style.textAlign = "center";
+    $linkDOM.style.marginLeft = "auto";
+    $linkDOM.style.marginRight = "auto";
+    $linkDOM.style.padding = "1rem";
+    $linkDOM.style.borderRadius = ".5rem";
+
+    console.log($linkDOM.style);
+    console.log($linkDOM.getAttribute("style"));
+    console.log(getComputedStyle($linkDOM));
+
+    //Variables CSS - Custom Properties CSS
+    
+    const $html = document.documentElement,
+    $body = document.body;
+
+    let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color"),
+    varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color")
+
+    console.log(varDarkColor, varYellowColor);
+
+    $body.style.backgroundColor = varDarkColor;
+    $body.style.color = varYellowColor;
+
+    $html.style.setProperty("--dark-color", "#333");
+    varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+
+    $body.style.setProperty("background-color", varDarkColor);
+
+    console.groupEnd();
+}
