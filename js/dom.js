db5deee3810df5d2d9964268e46ffd6a0e4e66cc
criZorr <christophermjk@outlook.com>
2023-07-20 15:48:26 -0500

    17 - BOM: Propiedades y Eventos

--- js/dom.js
+++ js/dom.js
@@ -527,3 +527,46 @@
 
     console.groupEnd();
 }
+
+//BOM: Propiedades y Eventos
+{
+    console.group("17 - BOM: Propiedades y Eventos");
+    document.body.style.setProperty("color", "yellow");
+
+    window.addEventListener("resize", (e) => {
+        console.clear();
+        console.log("*****Event Resize*****");
+        console.log(window.innerWidth);
+        console.log(window.innerHeight);
+        console.log(window.outerWidth);
+        console.log(window.outerHeight);
+        console.log(e);
+    });
+
+    window.addEventListener("scroll", e => {
+        //console.clear();
+        console.log("*****Event Scroll*****");
+        console.log(window.scrollX);
+        console.log(window.scrollY);
+        console.log(e);
+    });
+
+    window.addEventListener("load", e => {
+        //console.clear();
+        console.log("*****Event Load*****");
+        console.log(window.screenX);
+        console.log(window.screenY);
+        console.log(e);
+        
+    });
+
+    document.addEventListener("DOMContentLoaded", e => {
+        //console.clear();
+        console.log("*****Event DOMContentLoaded*****");
+        console.log(window.screenX);
+        console.log(window.screenY);
+        console.log(e);
+    });
+
+    console.groupEnd();
+}
