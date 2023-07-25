015f52e39ea68099b6d98e341d2ffd625e8a30de
criZorr <christophermjk@outlook.com>
2023-07-25 17:03:40 -0500

    02 - Ejercicio DOM: Reloj digital

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,7 +1,12 @@
 import hamburguerMenu from "./dom/01_menu_hamburguesa.js";
+import reloj from "./dom/02_alarma_sonora.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburguerMenu(".panel-btn", ".panel", ".menu a");
+    reloj("start", "#btn-inciar-r", "btns-reloj");
+    reloj("stop", "#btn-detener-r", "btns-reloj");
+    reloj("start", "#btn-inciar-a", "alarma");
+    reloj("stop", "#btn-detener-a", "alarma");
 });
