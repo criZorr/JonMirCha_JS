2aaa0208f05707fc29beaab6a17778019e6bf526
criZorr <christophermjk@outlook.com>
2023-08-03 17:34:22 -0500

    07 - Correción ejercicios DOM: Botón de scroll

--- js/index_dom.js
+++ js/index_dom.js
@@ -2,7 +2,7 @@ import hamburguerMenu from "./dom/01_menu_hamburguesa.js";
 import { relojDigital, alarm } from "./dom/02_reloj.js";
 import { moveBall, shortcuts } from "./dom/03_teclado.js";
 import { countdown } from "./dom/04_countdown.js";
-import { scrollBtn } from "./dom/05_scroll_btn.js";
+import scrollBtn from "./dom/05_scroll_btn.js";
 
 const d = document;
 
