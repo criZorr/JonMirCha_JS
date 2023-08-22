a86b2948bec21d20885d7f860db7b522db6f8935
criZorr <christophermjk@outlook.com>
2023-08-22 12:14:13 -0500

    v4.9.0 - Correción ejercicio 09 DOM: Responsive Tester

--- js/dom/08_responsive_tester.js
+++ js/dom/08_responsive_tester.js
@@ -1,21 +1,23 @@
 const d =document,
 w = window;
 
-export default function responsiveTester(url, width, height, openBtn, closeBtn) {
-    let $url = d.getElementById(url),
-    $width = d.getElementById(width),
-    $height = d.getElementById(height),
-    x = (screen.width - $width.value)/2,
-    y = (screen.height - $height.value)/2,
-    ventana,
-    ventanaPropeties = `popup, width=${$width.value}, height=${$height.value}, menubar=no, left=${x}, top=${y}`;
+export default function responsiveTester(form) {
+    const $form = d.getElementById(form);
 
-    d.addEventListener("click", e => {
-        if (e.target.matches(openBtn)) {
-            ventana = w.open($url.value,undefined,ventanaPropeties);
+    let ventana;
+
+    d.addEventListener("submit", e => {
+        if (e.target === $form) {
+            e.preventDefault();
+            let x = (screen.width - $form.width.value)/2,
+            y = (screen.height - $form.height.value)/2;
+
+            ventana = w.open($form.direction.value,"tester",`innerWidth=${$form.width.value}, innerHeight=${$form.height.value}, left=${x}, top=${y}`);
         }
-
-        if (e.target.matches(closeBtn)) {
+    });
+    
+    d.addEventListener("click", e=>{
+        if (e.target === $form.cerrar) {
             ventana.close();
         }
     });
