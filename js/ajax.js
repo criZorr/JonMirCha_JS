ea182efd88d436dc6c5f4d17ff6eb1affb3648d7
criZorr <christophermjk@outlook.com>
2023-10-10 16:15:39 -0500

    04 - AJAX: Librería Axios

--- js/ajax.js
+++ js/ajax.js
@@ -71,7 +71,7 @@
 
   async function getData() {
     try {
-      let res = await fetch("https://jsonplaceholder.typicode.com/user"),
+      let res = await fetch("https://jsonplaceholder.typicode.com/users"),
         json = await res.json();
 
       console.log(res, json);
@@ -95,4 +95,32 @@
     }
   }
   getData();
+})();
+
+(() => {
+  const $axios = document.getElementById("axios"),
+    $fragment = document.createDocumentFragment();
+
+  axios
+    .get("https://jsonplaceholder.typicode.com/user")
+    .then((res) => {
+      console.log(res);
+      let json = res.data;
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $axios.appendChild($fragment);
+    })
+    .catch((err) => {
+      console.log(err.response);
+      let message = err.response.statusText || "Ocurrió un error";
+      $axios.innerHTML = `Error: ${err.response.status}: ${message}`;
+    })
+    .finally(() => {
+      console.log("Esto se ejecutará independientemenete del resultado Axios");
+    });
 })();
