0456697bfc1c1a0ba88dda6311c0660756948d44
criZorr <christophermjk@outlook.com>
2023-10-10 14:45:30 -0500

    02 - AJAX: API Fetch

--- js/ajax.js
+++ js/ajax.js
@@ -32,3 +32,35 @@
 
   xhr.send();
 })();
+
+(() => {
+  const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    // .then((res) => {
+    //   console.log(res);
+    //   return res.ok ? res.json() : Promise.reject(res);
+    // })
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      console.log(json);
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetch.appendChild($fragment);
+    })
+    .catch((err) => {
+      console.log(err);
+      let message = err.statusText || "Ocurrió un error";
+      $fetch.innerHTML = `Error: ${err.status}: ${message}`;
+    })
+    .finally(() =>
+      console.log(
+        "Esto se ejecutará independientemente del resultado de la Promesa Fetch"
+      )
+    );
+})();
