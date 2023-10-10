a4acde2c445b0435be429eece07ae259697fc736
criZorr <christophermjk@outlook.com>
2023-10-10 15:18:47 -0500

    03 - AJAX: API Fetch + Async-Await

--- js/ajax.js
+++ js/ajax.js
@@ -64,3 +64,35 @@
       )
     );
 })();
+
+(() => {
+  const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await fetch("https://jsonplaceholder.typicode.com/user"),
+        json = await res.json();
+
+      console.log(res, json);
+
+      //   if (!res.ok) throw new Error("Ocurrió un error al solicitar los datos");
+      if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetchAsync.appendChild($fragment);
+    } catch (err) {
+      console.log(err);
+      let message = err.statusText || "Ocurrió un error";
+      $fetchAsync.innerHTML = `Error: ${err.status}: ${message}`;
+    } finally {
+      console.log("Esto se ejecutará independientemente del try... catch");
+    }
+  }
+  getData();
+})();
