aad8515ba343aa26153bbc5819b311da6d09fff0
criZorr <christophermjk@outlook.com>
2023-10-11 14:24:50 -0500

    05 - AJAX: Libreria Axios + Async-Await

--- js/ajax.js
+++ js/ajax.js
@@ -102,7 +102,7 @@
     $fragment = document.createDocumentFragment();
 
   axios
-    .get("https://jsonplaceholder.typicode.com/user")
+    .get("https://jsonplaceholder.typicode.com/users")
     .then((res) => {
       console.log(res);
       let json = res.data;
@@ -123,4 +123,33 @@
     .finally(() => {
       console.log("Esto se ejecutará independientemenete del resultado Axios");
     });
+})();
+
+(() => {
+  const $axiosAsync = document.getElementById("axios-async"),
+    $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await axios.get("https://jsonplaceholder.typicode.com/users"),
+        json = await res.data;
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $axiosAsync.appendChild($fragment);
+
+      console.log(res, json);
+    } catch (err) {
+      console.log(err.response);
+      let message = err.response.statusText || "Ocurrió un error";
+      $axiosAsync.innerHTML = `Error: ${err.response.status}: ${message}`;
+    } finally {
+      console.log("Esto se ejecutará independientemente del try... catch");
+    }
+  }
+  getData();
 })();
