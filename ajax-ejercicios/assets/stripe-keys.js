3955c35747181f5545830f68e67b9f1be0518cfd
criZorr <christophermjk@outlook.com>
2023-11-13 16:11:07 -0500

    Ejercicio AJAX 08 - Pagos Online con Fetch y Stripe

---
+++ ajax-ejercicios/assets/stripe-keys.js
@@ -0,0 +1,6 @@
+export default {
+  public:
+    "pk_test_51OAFKSBo9ALRg4EgJ3VNhX0Jn0ZVxMwPZMZKZcBCO5XzKX4parVxEFdNOWFm97hvwjeEOnBV8wqyB2Q9ZdMKa6m40069Ybmj7i",
+  secret:
+    "sk_test_51OAFKSBo9ALRg4EgSNw6IZ8d1zins6DBzYAZDFGBLMwaDVAnvr34Jp6uvNE4broTgFG64sKdtvZmq8POi3O5Q7D500mPtVrCvN",
+};
