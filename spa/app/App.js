cdd4666f2c8274dca4db9a23df383136cfdb9ae0
criZorr <christophermjk@outlook.com>
2024-01-04 14:50:51 -0500

    05 - SPA: Componente Header

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,11 +1,11 @@
 import api from "./helpers/wp_api.js";
 import { ajax } from "./helpers/ajax.js";
-import { Title } from "./components/Title.js";
 import { Loader } from "./components/Loader.js";
+import { Header } from "./components/Header.js";
 
 export function App() {
   const d = document,
     $root = d.getElementById("root");
-  $root.appendChild(Title());
+  $root.appendChild(Header());
   $root.appendChild(Loader());
 }
---
+++ spa/app/components/SearchForm.js
@@ -0,0 +1,12 @@
+export function SearchForm() {
+  const $form = document.createElement("form"),
+    $input = document.createElement("input");
+
+  $form.classList.add("form-search");
+  $input.name = "search";
+  $input.type = "search";
+  $input.placeholder = "Buscar...";
+
+  $form.appendChild($input);
+  return $form;
+}
