a0aca7d10a40d311f1a3c9ec918211c6f8ee12f1
criZorr <christophermjk@outlook.com>
2024-01-04 14:17:25 -0500

    04 - SPA: Primeros Componentes de UI

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,24 +1,11 @@
 import api from "./helpers/wp_api.js";
 import { ajax } from "./helpers/ajax.js";
+import { Title } from "./components/Title.js";
+import { Loader } from "./components/Loader.js";
 
 export function App() {
-  document.getElementById(
-    "root"
-  ).innerHTML = `<h1>Bienvenidos a mi primer SPA con vanilla JS</h1>`;
-
-  console.log(api);
-
-  ajax({
-    url: api.POSTS,
-    cbSuccess: (posts) => {
-      console.log(posts);
-    },
-  });
-
-  ajax({
-    url: api.CATEGORIES,
-    cbSuccess: (categories) => {
-      console.log(categories);
-    },
-  });
+  const d = document,
+    $root = d.getElementById("root");
+  $root.appendChild(Title());
+  $root.appendChild(Loader());
 }
