c961231b9f76b1393a0ac313bd72ae296d9381a5
criZorr <christophermjk@outlook.com>
2024-01-04 17:21:30 -0500

    08 - SPA: Refactorización de la App. Enrutamiento Asíncrono

--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -2,30 +2,30 @@ import { ajax } from "../helpers/ajax.js";
 import api from "../helpers/wp_api.js";
 import { PostCard } from "./PostCard.js";
 
-export function Router() {
+export async function Router() {
   const d = document,
     w = window,
-    $posts = d.getElementById("posts");
+    $main = d.getElementById("main");
 
   let { hash } = location;
 
-  $posts.innerHTML = null;
+  $main.innerHTML = null;
 
   if (!hash || hash === "#/") {
-    ajax({
+    await ajax({
       url: api.POSTS,
       cbSuccess: (posts) => {
         let html = "";
         posts.forEach((post) => (html += PostCard(post)));
         d.querySelector(".loader").style.display = "none";
-        $posts.innerHTML = html;
+        $main.innerHTML = html;
       },
     });
   } else if (hash.includes("#/search")) {
-    $posts.innerHTML = "<h2>Sección del Buscador</h2>";
+    $main.innerHTML = "<h2>Sección del Buscador</h2>";
   } else if (hash === "#/contacto") {
-    $posts.innerHTML = "<h2>Sección del Contacto</h2>";
+    $main.innerHTML = "<h2>Sección del Contacto</h2>";
   } else {
-    $posts.innerHTML = "<h2>Aquí cargará el contenido del post</h2>";
+    $main.innerHTML = "<h2>Aquí cargará el contenido del post</h2>";
   }
 }
