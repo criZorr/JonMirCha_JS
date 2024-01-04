88adbaa679f0c22772a444ec662a3f0b595176d9
criZorr <christophermjk@outlook.com>
2024-01-04 11:15:08 -0500

    02 - SPA: Helper de conexión a API REST

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,5 +1,9 @@
+import api from "./helpers/wp_api.js";
+
 export function App() {
   document.getElementById(
     "root"
   ).innerHTML = `<h1>Bienvenidos a mi primer SPA con vanilla JS</h1>`;
+
+  console.log(api);
 }
---
+++ spa/app/helpers/wp_api.js
@@ -0,0 +1,19 @@
+//https://developer.wordpress.org/rest-api/
+
+const NAME = "malvestida",
+  DOMAIN = `https://${NAME}.com`,
+  SITE = `${DOMAIN}/wp-json`,
+  API_WP = `${SITE}/wp/v2`,
+  POSTS = `${API_WP}/posts?_embed`,
+  POST = `${API_WP}/posts`,
+  SEARCH = `${API_WP}/search?_embed&search=`;
+
+export default {
+  NAME,
+  DOMAIN,
+  SITE,
+  API_WP,
+  POSTS,
+  POST,
+  SEARCH,
+};
