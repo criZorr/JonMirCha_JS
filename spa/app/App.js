92e8f694f429a2bb2eef039917d548767335c598
criZorr <christophermjk@outlook.com>
2024-01-04 15:57:59 -0500

    06 - SPA: Renderizado Dinámico

--- spa/app/App.js
+++ spa/app/App.js
@@ -2,10 +2,24 @@ import api from "./helpers/wp_api.js";
 import { ajax } from "./helpers/ajax.js";
 import { Loader } from "./components/Loader.js";
 import { Header } from "./components/Header.js";
+import { Posts } from "./components/Posts.js";
+import { PostCard } from "./components/PostCard.js";
 
 export function App() {
   const d = document,
     $root = d.getElementById("root");
   $root.appendChild(Header());
+  $root.appendChild(Posts());
   $root.appendChild(Loader());
+
+  ajax({
+    url: api.POSTS,
+    cbSuccess: (posts) => {
+      console.log(posts);
+      let html = "";
+      posts.forEach((post) => (html += PostCard(post)));
+      d.querySelector(".loader").style.display = "none";
+      d.getElementById("posts").innerHTML = html;
+    },
+  });
 }
